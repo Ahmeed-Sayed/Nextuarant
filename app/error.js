a28372636f4e403a375b83ef049f428a17922b05
Ahmeed-Sayed <ahmeedsayed01@gmail.com>
2024-11-04 18:43:12 +0200

    feat: added error handling page via error.js file

---
+++ app/error.js
@@ -0,0 +1,11 @@
+"use client";
+function ErrorPage() {
+  return (
+    <main className="error">
+      <h1>An error has occeured</h1>
+      <p>Please try again later.</p>
+    </main>
+  );
+}
+
+export default ErrorPage;
--- utils/meals.js
+++ utils/meals.js
@@ -2,8 +2,9 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals() {
-  await new Promise((resolve, reject) => setTimeout(resolve, 5000));
+  await new Promise((resolve, reject) => setTimeout(resolve, 2000));
   // .get returns sngle row
   //.run inserts into the database
+  throw new Error("Not implemented");
   return db.prepare("SELECT * FROM meals").all();
 }
