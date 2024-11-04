1bcffa6c102f03eb01d54223f2929f3fd38e5201
Ahmeed-Sayed <ahmeedsayed01@gmail.com>
2024-11-04 19:33:08 +0200

    feat: added share meal page and an image picker component

---
+++ components/meals/image-picker.js
@@ -0,0 +1,54 @@
+"use client";
+import { useRef, useState } from "react";
+import classes from "./image-picker.module.css";
+import Image from "next/image";
+function ImagePicker({ name }) {
+  const inputRef = useRef();
+  const [pickedImage, setPickedImage] = useState(null);
+  function handlePickerClick() {
+    inputRef.current.click();
+  }
+  function handleFileChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      setPickedImage(event.target.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  return (
+    <main className={classes.picker}>
+      <div className={classes.control}>
+        <div className={classes.preview}>
+          {pickedImage ? (
+            <Image src={pickedImage} alt="picked image" fill />
+          ) : (
+            <p>No image picked yet.</p>
+          )}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          accept="image/png, image/jpeg,"
+          id={name}
+          name={name}
+          ref={inputRef}
+          onChange={handleFileChange}
+        />
+        <button
+          type="button"
+          onClick={handlePickerClick}
+          className={classes.button}
+        >
+          Pick Your Image
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default ImagePicker;
